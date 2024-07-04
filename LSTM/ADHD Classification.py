6e8a8158c8a90252373ab40be3bba88b776ea0d5
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-07-04 15:33:46 +0900

    adhd_LSTM commit

--- LSTM/ADHD Classification.py
+++ LSTM/ADHD Classification.py
@@ -1,19 +1,29 @@
-from nilearn import plotting
+from keras.api import optimizers
+from keras.src.layers import LSTM, Dense
+from keras.src.utils import plot_model
 from nilearn import datasets
-from nilearn import image
+from nilearn import image as img
+from nilearn import plotting
 from nilearn.input_data import NiftiMapsMasker
 import numpy as np
-import keras
 from keras import utils, Sequential
 from sklearn.model_selection import train_test_split
-from torch.nn import LSTM
 
-# Smith's re-fMRI component atlas 를 적용
+# 뇌를 smith_atlas 에 따라 70개의 구역 으로 나눈다.
 
 smith_atlas = datasets.fetch_atlas_smith_2009()
 smith_atlas_rs_networks = smith_atlas.rsn70
 
+# Showing results
+plotting.plot_prob_atlas(smith_atlas_rs_networks,
+                         title='Smith atlas',
+                         colorbar=True)
+plotting.show()
+
+# Import 40 subjects from a preprocessed, ready-to-go, dataset
 adhd_data = datasets.fetch_adhd(n_subjects=40)
+
+print(len(adhd_data['func']))
 
 # Generate a mask
 masker = NiftiMapsMasker(maps_img=smith_atlas_rs_networks,  # Smith atlas
@@ -21,10 +31,10 @@ masker = NiftiMapsMasker(maps_img=smith_atlas_rs_networks,  # Smith atlas
                          memory='nilearn_cache',  # cache
                          verbose=0)  # do not print verbose
 
-all_subjects_data = []
-labels = []
+# Apply the mask on the data & sort it based on label
 
-print("done")
+all_subjects_data = []  # actiavtion patterns for all subjects
+labels = []  # 1 if ADHD, 0 if control
 
 for func_file, confound_file, phenotypic in zip(
         adhd_data.func, adhd_data.confounds, adhd_data.phenotypic):
@@ -33,18 +43,16 @@ for func_file, confound_file, phenotypic in zip(
     all_subjects_data.append(time_series)
     labels.append(phenotypic['adhd'])
 
-# labels 에는 실제 데이터가 들어있다.
-
 print('N control:', labels.count(0))
 print('N ADHD:', labels.count(1))
 
-max_len_image = np.max([len(i) for i in all_subjects_data])
+# 40명의 데이터는 어쩌면 동일한 time stamp 를 가지고 있지 않을 수 있다. 그러면 데이터가 통일성 이 없어서 모델이 잘 작동 하지 않는다.
 
-# Data might not hold homogenous scan, some of them might not be in same time length with others
+# get longest scan
+max_len_image = np.max([len(i) for i in all_subjects_data])
 
 # reshape all data
 all_subjects_data_reshaped = []
-
 for subject_data in all_subjects_data:
     # Padding
     N = max_len_image - len(subject_data)
@@ -58,17 +66,11 @@ for subject_data in all_subjects_data:
 print('data shape: ', np.array(all_subjects_data_reshaped).shape)
 
 
+# Data preparation
+
 def get_train_test(X, y, i, verbose=False):
-    """
-    split to train and test and reshape data
-
-    X data
-    y labels
-    i random state
-    """
-
     X_train, X_test, y_train, y_test = train_test_split(X,
-                                                        y, test_size=0.1, random_state=i)
+                                                        y, test_size=0.2, random_state=i)
 
     # Reshapes data to 3D for Hierarchical RNN.
     t_shape = np.array(all_subjects_data_reshaped).shape[1]
@@ -92,21 +94,20 @@ def get_train_test(X, y, i, verbose=False):
 
     return X_train, X_test, y_train, y_test
 
-
 # create the model
 
 model = Sequential()
 
 # LSTM layers -
 # Long Short-Term Memory layer - Hochreiter 1997.
-t_shape = np.array(all_subjects_data_reshaped).shape[1]
-RSN_shape = np.array(all_subjects_data_reshaped).shape[2]
+t_shape=np.array(all_subjects_data_reshaped).shape[1]
+RSN_shape=np.array(all_subjects_data_reshaped).shape[2]
 
-model.add(LSTM(units=70,  # dimensionality of the output space
-               dropout=0.4,  # Fraction of the units to drop (inputs)
-               recurrent_dropout=0.15,  # Fraction of the units to drop (recurent state)
-               return_sequences=True,  # return the last state in addition to the output
-               input_shape=(t_shape, RSN_shape)))
+model.add(LSTM(units=70, # dimensionality of the output space
+               dropout=0.4, # Fraction of the units to drop (inputs)
+               recurrent_dropout=0.15, # Fraction of the units to drop (recurent state)
+               return_sequences=True, # return the last state in addition to the output
+               input_shape=(t_shape,RSN_shape)))
 
 model.add(LSTM(units=60,
                dropout=0.4,
@@ -123,12 +124,13 @@ model.add(LSTM(units=40,
                recurrent_dropout=0.15,
                return_sequences=False))
 
+
 model.add(Dense(units=2,
                 activation="sigmoid"))
 
 model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.Adam(lr=0.001),
-              metrics=['binary_accuracy'])
+                optimizer=optimizers.Adam(lr=0.001),
+                metrics=['binary_accuracy'])
 
 #print(model.summary())
 plot_model(model, show_shapes=True, show_layer_names=True)
