f29ea3f5eaa34dc7802f5b42c5d5650f8018ca95
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-07-30 14:47:47 +0900

    SVC_07.30 commit

--- SVM/Jun_data.py
+++ SVM/Jun_data.py
@@ -20,12 +20,15 @@ r2 = Functional_Connectivity.RBD_FC('/Users/oj/Desktop/post_fMRI_RBD/sub-02/ses-
 h1 = Functional_Connectivity.HC_FC('/Users/oj/Desktop/pre_BIDS/BIDS_HC_final/sub-02/func/warsub-02_task-RESEARCHMRI_acq-AxialfMRIrest_bold.nii',
                                     '/Users/oj/Desktop/pre_BIDS/BIDS_HC_final/sub-02/func/rp_sub-02_task-RESEARCHMRI_acq-AxialfMRIrest_bold.tsv')
 
+h2 = Functional_Connectivity.HC_FC('/Users/oj/Desktop/pre_BIDS/BIDS_HC_final/sub-03/func/warsub-03_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_rec-BRAINMRINONCONTRASTDIFFUSION_bold.nii',
+                                    '/Users/oj/Desktop/pre_BIDS/BIDS_HC_final/sub-03/func/rp_sub-03_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_rec-BRAINMRINONCONTRASTDIFFUSION_bold.tsv')
 
 rbd.append(np.array(r1.flatten()))
 rbd.append(np.array(r2.flatten()))
 
 hc.append(np.array(h1.flatten()))
-hc.append(np.array(h1.flatten()))
+hc.append(np.array(h2.flatten()))
+
 
 
 for j in rbd:
--- SVM/SVM.py
+++ SVM/SVM.py
@@ -11,6 +11,7 @@ jun_df = Jun_data.jun_df.apply(pd.to_numeric, errors='coerce')
 X = np.array(Jun_data.jun_df['FC'].tolist())
 y = np.array(Jun_data.jun_df['Status'].tolist())
 
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 svm_model = svm.SVC(kernel='linear')
@@ -18,6 +19,8 @@ svm_model.fit(X_train, y_train)
 
 # 테스트 데이터에 대한 예측
 y_pred = svm_model.predict(X_test)
+
+print(y_train)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
 print("Classification Report:")
