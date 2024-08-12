c41d7a305a1042ed363076866dc631ef5761a9bc
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-08-12 14:37:38 +0900

    DATA_08.12_FC commit

--- Data/pilot_data.py
+++ Data/pilot_data.py
@@ -18,13 +18,20 @@ pilot_reho = pd.read_csv(
     '/Users/oj/Desktop/post_fMRI/xcp_d_HC/sub-03/func/sub-03_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_space-MNI152NLin2009cAsym_seg-Tian_stat-reho_bold.tsv',
     sep='\t')
 
-print(pilot_reho)
+pilot_alff = pd.read_csv('/Users/oj/Desktop/post_fMRI/xcp_d_HC/sub-03/func/sub-03_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_space-MNI152NLin2009cAsym_seg-Tian_stat-alff_bold.tsv',
+                         sep='\t')
+
+print(pilot_alff)
 
 
 reho.append(np.array(pilot_reho.iloc[0]))
+alff.append(np.array(pilot_alff.iloc[0]))
 
 
 for j in reho:
     pilot_data = pd.concat([pilot_data, pd.DataFrame({'REHO':[j], 'Status':[0]})], ignore_index=True)
+
+for j in alff:
+    pilot_data = pd.concat([pilot_data, pd.DataFrame({'ALFF': [j], 'Status': [0]})], ignore_index=True)
 
 print(pilot_data.head())
