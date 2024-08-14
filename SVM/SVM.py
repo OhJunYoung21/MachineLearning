0af5d6083c70cd955365567a457cede1c271eb5a
OhJunYoung21 <dhwnsdyd21@gmail.com>
2024-08-14 10:25:49 +0900

    SVM_08.13 commit

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -2,26 +2,18 @@ import numpy as np
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,accuracy_score
-import Functional_Connectivity
-import Jun_data
-import pandas as pd
+from Data.pilot_data import pilot_data
 
-jun_df = Jun_data.jun_df.apply(pd.to_numeric, errors='coerce')
+data = pilot_data
 
-X = np.array(Jun_data.jun_df['FC'].tolist())
-y = np.array(Jun_data.jun_df['Status'].tolist())
-
+X = pilot_data['FC']
+y = pilot_data['STATUS']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-svm_model = svm.SVC(kernel='linear')
-svm_model.fit(X_train, y_train)
+model = svm.SVC(kernel='rbf')
+model.fit(X_train, y_train)
 
-# 테스트 데이터에 대한 예측
-y_pred = svm_model.predict(X_test)
+accuracy = model.score(X_test, y_test)
+print("Accuracy:", accuracy)
 
-print(y_train)
-
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Classification Report:")
-print(classification_report(y_test, y_pred))
