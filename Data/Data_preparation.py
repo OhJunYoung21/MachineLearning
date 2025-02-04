fe83fc7a4b7b190328d42d6c9178b16b6acc781a
OhJunYoung21 <dhwnsdyd21@gmail.com>
2025-02-04 14:59:11 +0900

    GNN corr,pcorr,avg_pcorr 제작 commit

--- Data/Data_preparation.py
+++ Data/Data_preparation.py
@@ -15,6 +15,8 @@ root_dir = '/Users/oj/Desktop/Yoo_Lab/Yoo_data/RBD_PET_positive'
 
 atlas_path = '/Users/oj/Desktop/Yoo_Lab/atlas/shen_2mm_268_parcellation.nii'
 
+avg_pcorr_file = f'{root_dir}/GNN/avg_pcorr.csv'
+
 fMRI_img = glob.glob(os.path.join(root_dir, 'sub-*', 'func',
                                   'sub-*_task-BRAINMRINONCONTRASTDIFFUSION_acq-AxialfMRIrest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz')) + glob.glob(
     os.path.join(root_dir, 'sub-*', 'func',
@@ -44,7 +46,9 @@ raw_confounds = glob.glob(os.path.join(root_dir, 'sub-*', 'func',
 raw_confounds = sorted(raw_confounds)
 fMRI_img = sorted(fMRI_img)
 
-print(len((fMRI_img)), len(raw_confounds))
+shen_atlas = input_data.NiftiLabelsMasker(labels_img=atlas_path, standardize=True, memotry='nilearn_cache')
+
+time_series_list = []
 
 for index in range(len(raw_confounds)):
     fmri_img = fMRI_img[index]
@@ -53,13 +57,21 @@ for index in range(len(raw_confounds)):
     confounds.replace([np.inf, -np.inf], np.nan, inplace=True)
     confounds.fillna(0, inplace=True)
 
-    shen_atlas = input_data.NiftiLabelsMasker(labels_img=atlas_path, standardize=True, memotry='nilearn_cache')
-
     data = image.load_img(fmri_img)
 
     time_series = shen_atlas.fit_transform(data, confounds=confounds)
 
-    subject_number = re.search(r'sub-(\d+)', fMRI_img[index]).group(1)
-
-    np.savetxt(f'/Users/oj/Desktop/Yoo_Lab/Yoo_data/RBD_PET_positive/GNN/time_series_{index:02d}.csv', time_series,
+    time_series_list.append(time_series)
+    '''
+    np.savetxt(f'/Users/oj/Desktop/Yoo_Lab/Yoo_data/RBD_PET_positive/GNN/time_series/time_series_{index + 1:02d}.csv',
+               time_series,
                delimiter=',')
+    '''
+
+corr_measure = ConnectivityMeasure(kind='correlation')
+pcorr_measure = ConnectivityMeasure(kind='partial correlation')
+
+corr_matrices = corr_measure.fit_transform(time_series_list)
+pcorr_matrices = pcorr_measure.fit_transform(time_series_list)
+
+print(corr_matrices[0].shape)
